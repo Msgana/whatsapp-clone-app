6e0e4bad6bed4917b6515c59d0dc369cc649d015
misgana <misganayohannes@gmail.com>
2020-12-28 23:44:39 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXTPL6WDGl5wtvKnilhnmLmKFXpiBvy18",
-  authDomain: "whatsapp-d5c6e.firebaseapp.com",
-  projectId: "whatsapp-d5c6e",
-  storageBucket: "whatsapp-d5c6e.appspot.com",
-  messagingSenderId: "394932372101",
-  appId: "1:394932372101:web:bda3d98a861f4cc1879fec",
-  measurementId: "G-E5E3D5M0JL",
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " ",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
